bfc6f7c1bedb71bb9e681e2742cba11aae10b212
Valentin poli <valentin.poli@epita.fr>
2021-02-18 14:55:52 +0000

    Ajoutez des fichiers projet.

---
+++ StudentApplication/Controllers/StudentsController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using StudentApplication.Models;
+
+namespace StudentApplication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentsController : ControllerBase
+    {
+        public static List<Students> GetStudents()
+        {
+            List<Students> students = new List<Students>();
+            students.Add(new Students() { Id = 1, First_name = "Valentin", Last_name = "Poli", Age = 19 });
+            students.Add(new Students() { Id = 2, First_name = "Valentin", Last_name = "Poli", Age = 19 });
+            students.Add(new Students() { Id = 3, First_name = "Valentin", Last_name = "Poli", Age = 19 });
+            students.Add(new Students() { Id = 4, First_name = "Valentin", Last_name = "Poli", Age = 19 });
+            students.Add(new Students() { Id = 5, First_name = "Valentin", Last_name = "Poli", Age = 19 });
+            return students;
+        }
+
+        [HttpGet]
+        public IEnumerable<Students> GetStudents_List()
+        {
+            return GetStudents();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Students> GetStudents_ById(int id)
+        {
+            var students = GetStudents().Find(x => x.Id == id);
+            if (students != null)
+            {
+                return students;
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ StudentApplication/Models/Students.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentApplication.Models
+{
+    public class Students
+    {
+        public int Id { get; set; }
+        public string First_name { get; set; }
+        public string Last_name { get; set; }
+        public int Age { get; set; }
+
+    }
+}
